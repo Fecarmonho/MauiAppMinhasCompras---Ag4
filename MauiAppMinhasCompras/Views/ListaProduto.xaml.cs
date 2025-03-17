feafb3ed0d700299ac02a1147f22b2fa34af98bc
Fecarmonho <fe.carmonho@gmail.com>
2025-03-17 10:18:23 -0300

    Finalização do básico do app - remover e selecionar itens da lsita

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,63 +1,113 @@
+
 using MauiAppMinhasCompras.Models;
 using System.Collections.ObjectModel;
-using System.Linq;
 
-namespace MauiAppMinhasCompras.Views
+namespace MauiAppMinhasCompras.Views;
+
+public partial class ListaProduto : ContentPage
 {
-    public partial class ListaProduto : ContentPage
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
+    public ListaProduto()
     {
-        ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+        InitializeComponent();
 
-        public ListaProduto()
-        {
-            InitializeComponent();
-            lst_produtos.ItemsSource = lista; 
-        }
+        lst_produtos.ItemsSource = lista;
+    }
 
-        protected async override void OnAppearing()
+    protected async override void OnAppearing()
+    {
+        try
         {
-            
+            lista.Clear();
+
             List<Produto> tmp = await App.Db.GetAll();
+
             tmp.ForEach(i => lista.Add(i));
         }
-
-        
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        catch (Exception ex)
         {
-            try
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto());
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            string q = e.NewTextValue;
+
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.Search(q);
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            MenuItem selecinado = sender as MenuItem;
+
+            Produto p = selecinado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert(
+                "Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+            if (confirm)
             {
-                Navigation.PushAsync(new Views.NovoProduto());
+                await App.Db.Delete(p.Id);
+                lista.Remove(p);
             }
-            catch (Exception ex)
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto;
+
+            Navigation.PushAsync(new Views.EditarProduto
             {
-                DisplayAlert("Ops", ex.Message, "OK");
-            }
+                BindingContext = p,
+            });
         }
-
-        
-        private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        catch (Exception ex)
         {
-            string query = e.NewTextValue; 
-            lista.Clear(); 
-            
-            List<Produto> tmp = string.IsNullOrEmpty(query)
-                ? await App.Db.GetAll() 
-                : await App.Db.Search(query); 
-
-            tmp.ForEach(i => lista.Add(i)); 
-        }
-
-        
-        private void ToolbarItem_Clicked_1(object sender, EventArgs e)
-        {
-            double soma = lista.Sum(i => i.Total); 
-            string msg = $"O total é {soma:C}";
-            DisplayAlert("Total dos Produtos", msg, "OK");
-        }
-
-        private void MenuItem_Clicked(object sender, EventArgs e)
-        {
-            
+            DisplayAlert("Ops", ex.Message, "OK");
         }
     }
 }
